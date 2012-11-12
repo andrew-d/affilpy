619859c8c1b95eaa376053ddc67ef390dc03d72b
Andrew Dunham <andrew@du.nham.ca>
2012-11-12 17:15:46 -0500

    Add iTunes affiliate linking

---
+++ affilpy/sites/itunes.py
@@ -0,0 +1,23 @@
+# Ref: http://www.apple.com/itunes/affiliates/resources/documentation/linking-to-the-itunes-music-store.html#apps
+
+class ItunesLink(object):
+    NAME = "iTunes"
+
+    def __init__(self, config, logger):
+        self.config = config
+        self.log = logger
+
+    def detect(self, f):
+        siteId = f.query.params.pop('siteID', None)
+        f.query.params.pop('partnerId')
+        return (siteId is not None), siteId, f
+
+    def add(self, f):
+        f.query.params['siteID'] = self.config[self.NAME + "_siteID"]
+        f.query.params['partnerId'] = self.config[self.NAME + "_partnerId"]
+
+
+registry = {
+    "itunes.apple.com": ItunesLink
+}
+
